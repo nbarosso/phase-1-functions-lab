ccf3709be935fe6383d4dfa799ff6b7b01d7bc42
Nicholas Barosso <nbarosso@gmail.com>
2023-08-12 12:31:45 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42)
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return Math.abs(pickupLocation - 42) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const realDistance = Math.abs(start - destination) * 264;
+    const fareDistance = realDistance - 400
+    if(realDistance <= 400) {
+        return 0
+    }
+    else if(realDistance > 400 && realDistance < 2000) {
+        return fareDistance * .02
+    }
+    else if(realDistance > 2000 && realDistance < 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
